cf1e0cbef849856bb9ccfcd9595ba81abb1d691e
Matt Ondo <yours.truly@mattondo.com>
2023-08-30 01:23:54 -0600

    adds analytics tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
 import "../faust.config";
-import React from "react";
+import { React } from "react";
 import { useRouter } from "next/router";
 import { FaustProvider } from "@faustwp/core";
+import Tracking from '../components/Tracking';
 import { WordPressBlocksProvider } from '@faustwp/blocks';
 import blocks from '../wp-blocks';
 import "../styles/globals.css";
@@ -10,13 +11,17 @@ export default function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
   return (
-    <FaustProvider pageProps={pageProps}>
-      <WordPressBlocksProvider
-        config={{
-          blocks,
-        }}>
-        <Component {...pageProps} key={router.asPath} />
-      </WordPressBlocksProvider>
-    </FaustProvider>
+    <>
+      <FaustProvider pageProps={pageProps}>
+        <WordPressBlocksProvider
+          config={{
+            blocks,
+          }}>
+          <Component {...pageProps} key={router.asPath} />
+        </WordPressBlocksProvider>
+      </FaustProvider>
+
+      <Tracking />
+    </>
   );
 }
