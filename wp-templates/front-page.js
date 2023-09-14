a55af777722215915872eccbf9b7825d941c35c1
Matt Ondo <yours.truly@mattondo.com>
2023-09-13 23:21:03 -0600

    use acf fields instead of gutenberg blocks

--- wp-templates/front-page.js
+++ wp-templates/front-page.js
@@ -1,4 +1,5 @@
 import { gql } from "@apollo/client";
+import Image from "next/image";
 import Head from "next/head";
 import Link from "next/link";
 import {
@@ -18,6 +19,25 @@ export default function Component(props) {
     props.data.generalSettings;
   const menuItems = props.data.primaryMenuItems.nodes;
   const heroContent = props.data.page.pageHeader;
+
+  const pageContent = {
+    section1: {
+      headline: props.data.page.homepageContent.hmsec1Headline,
+      content: props.data.page.homepageContent.hmsec1Content,
+      image: props.data.page.homepageContent.hmsec1Image,
+    },
+    section2: {
+      headline: props.data.page.homepageContent.hmsec2Headline,
+      content: props.data.page.homepageContent.hmsec2Content,
+      image: props.data.page.homepageContent.hmsec2Image,
+    },
+    section3: {
+      headline: props.data.page.homepageContent.hmsec3Headline,
+      content: props.data.page.homepageContent.hmsec3Content,
+      image: props.data.page.homepageContent.hmsec3Image,
+    }
+  }
+
 
   // Blocks
   const contentBlocks = props.data.page.editorBlocks;
@@ -41,7 +61,59 @@ export default function Component(props) {
       />
 
       <main className="container-fluid">
-        <WordPressBlocksViewer blocks={blocks}/>
+        <section className="flex flex-col md:flex-row-reverse items-center gap-8 my-20">
+          <div className="w-full">
+            <h2>{pageContent.section1.headline}</h2>
+            <p>{pageContent.section1.content}</p>
+          </div>
+          <div className="w-full">
+            <Image 
+              src={pageContent.section1.image.sourceUrl}
+              alt={pageContent.section1.image.altText}
+              sizes={pageContent.section1.image.sizes}
+              width={pageContent.section1.image.mediaDetails.width}
+              height={pageContent.section1.image.mediaDetails.height}
+              className="w-full rounded-lg"
+              priority={true}
+            />
+          </div>
+        </section>
+
+        <section className="flex flex-col md:flex-row items-center gap-8 my-20">
+          <div className="w-full">
+            <h2>{pageContent.section2.headline}</h2>
+            <p>{pageContent.section2.content}</p>
+          </div>
+          <div className="w-full">
+            <Image 
+              src={pageContent.section2.image.sourceUrl}
+              alt={pageContent.section2.image.altText}
+              sizes={pageContent.section2.image.sizes}
+              width={pageContent.section2.image.mediaDetails.width}
+              height={pageContent.section2.image.mediaDetails.height}
+              className="w-full rounded-lg"
+              priority={true}
+            />
+          </div>
+        </section>
+
+        <section className="flex flex-col md:flex-row-reverse items-center gap-8 my-20">
+          <div className="w-full">
+            <h2>{pageContent.section3.headline}</h2>
+            <p>{pageContent.section3.content}</p>
+          </div>
+          <div className="w-full">
+            <Image 
+              src={pageContent.section3.image.sourceUrl}
+              alt={pageContent.section3.image.altText}
+              sizes={pageContent.section3.image.sizes}
+              width={pageContent.section3.image.mediaDetails.width}
+              height={pageContent.section3.image.mediaDetails.height}
+              className="w-full rounded-lg"
+              priority={true}
+            />
+          </div>
+        </section>
       </main>
 
       <Footer />
@@ -72,9 +144,45 @@ Component.query = gql`
         renderedHtml
         ${getFragmentDataFromBlocks(blocks).keys}
       }
-      author {
-        node {
-          name
+      homepageContent {
+        hmsec1Headline
+        hmsec1Content
+        hmsec1Image {
+          uri
+          altText
+          srcSet
+          sourceUrl
+          sizes
+          mediaDetails {
+            width
+            height
+          }
+        }
+        hmsec2Headline
+        hmsec2Content
+        hmsec2Image {
+          uri
+          altText
+          srcSet
+          sourceUrl
+          sizes
+          mediaDetails {
+            width
+            height
+          }
+        }
+        hmsec3Headline
+        hmsec3Content
+        hmsec3Image {
+          uri
+          altText
+          srcSet
+          sourceUrl
+          sizes
+          mediaDetails {
+            width
+            height
+          }
         }
       }
     }
