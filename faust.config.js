f8468b726fbca04bbd1e46d581be83fa4410dea7
Matt Ondo <yours.truly@mattondo.com>
2023-09-14 20:06:12 -0600

    disable toolbar as it throws 401 error

--- faust.config.js
+++ faust.config.js
@@ -10,5 +10,5 @@ export default setConfig({
   experimentalPlugins: [],
   possibleTypes,
   usePersistedQueries: true,
-  experimentalToolbar: true, // Enable experimental toolbar
+  experimentalToolbar: false, 
 });
