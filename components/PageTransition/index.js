6860f156a25db3651145cb1a6e51b18be19702c7
Matt Ondo <yours.truly@mattondo.com>
2023-10-15 00:40:55 -0600

    finalize page transition

--- components/PageTransition/index.js
+++ components/PageTransition/index.js
@@ -2,15 +2,18 @@ import React, { forwardRef, useMemo } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 
 function PageTransition({ children, ...rest },ref) {
+	const onExitComplete = () => {
+		window.scrollTo({ top: 0 })
+	}
 	return (
-		<AnimatePresence mode="popLayout">
+		<AnimatePresence mode="wait" onExitComplete={onExitComplete}>
 			<motion.div
 				layout={true}
 				ref={ref}
 				key={rest.thekey}
-				initial={{ opacity: 0, y: 10 }}
+				initial={{ opacity: 0, y: -5 }}
 				animate={{ opacity: 1, y: 0 }}
-				exit={{ opacity: 0, y: -10 }}
+				exit={{ opacity: 0, y: 5 }}
 				transition={{
 					duration: 0.3,
 					delay: 0,
