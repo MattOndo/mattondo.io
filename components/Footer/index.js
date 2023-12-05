8056a86c3cfdd20fdf5dbea49937f05fa789b450
Matt Ondo <yours.truly@mattondo.com>
2023-12-04 18:40:20 -0700

    add link to privacy policy

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -3,7 +3,7 @@ import styles from "./style.module.css";
 export default function Footer() {
   return (
     <footer className="container-fluid text-center mt-20 mb-9">
-      <small>Copyright Matt Ondo</small>
+      <small>Copyright Matt Ondo | <a href="/privacy-policy/" className="text-lighter-gray font-sans">Privacy</a></small>
     </footer>
   );
 }
